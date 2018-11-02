4ad9c5322ff4e0e808c6513a5e51da03ccdcf978
Jonathon Shields <jonathonshields@gmail.com>
2018-11-03 11:25:42 +1300

    test

--- src/modules/Utility.js
+++ src/modules/Utility.js
@@ -1,11 +1,11 @@
 const request = require('request');
-const localforage = require('localforage');
+// const localforage = require('localforage');
 
 function getBirds(callback) {
     if(localStorage.getItem('birdDB')){
         callback(JSON.parse(localStorage.getItem('birdDB')));
     } else {
-        request('http://localhost:3000/birds.json', function (error, response, body){
+        request(window.location.host+'/birds.json', function (error, response, body){
             localStorage.setItem('birdDB',body);
             callback(JSON.parse(body));
         })
