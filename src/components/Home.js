389535195e757cd66766a6ef5aa21f800621198d
Jonathon Shields <jonathonshields@gmail.com>
2018-11-03 11:21:10 +1300

    filter and mo css

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,11 +9,16 @@ class Home extends Component {
             self.setState({ data: birds});
         })
     }
+    filterList = (event) =>{
+        this.setState({ filter : event.target.value.toLowerCase()});
+    }
     render() {
+      const filter = '';
       return (
           <div>
+            <input className='search' type='text' onChange={this.filterList}/>
             {this.state && this.state.data &&
-                <BirdList birds={this.state.data} />
+                <BirdList birds={this.state.data} filter={this.state.filter}/>
             }
           </div>
       )}
@@ -24,11 +29,18 @@ class BirdRow extends React.Component {
       const name = this.props.name;
       const scientific = this.props.scientific;
       const url = this.props.url;
+      const imagelink = this.props.imagelink;
       return (
         <Link to={`/species/${url}`}>
+            
             <div className='birdListItem'>
-                <h4>{name}</h4>
-                <p>{scientific}</p>
+                <div className='birdListImage'>
+                    <img src={imagelink}/>
+                </div>
+                <div className='birdListItemText'>
+                    <h4>{name}</h4>
+                    <p>{scientific}</p>
+                </div>
             </div>
         </Link>
       );
@@ -38,7 +50,15 @@ class BirdRow extends React.Component {
 class BirdList extends React.Component {
     render() {
         const rows = [];
-        const birdList = this.props.birds.sort(function (a, b) {
+        var birdList = this.props.birds;
+        const filter = this.props.filter;
+        if(filter && filter.length>0){
+            birdList = birdList.filter(function(item){
+                return item.name.toLowerCase().search(
+                  filter.toLowerCase()) !== -1;
+              });
+        }
+        birdList = birdList.sort(function (a, b) {
             return ('' + a.name).localeCompare(b.name);
         })
         birdList.forEach((bird) => {
@@ -46,7 +66,8 @@ class BirdList extends React.Component {
                 <BirdRow
                 name={bird.name}
                 scientific={bird.scientific}
-                url={bird.url}/>
+                url={bird.url}
+                imagelink={bird.imagelink}/>
             );
         });
         return rows;
